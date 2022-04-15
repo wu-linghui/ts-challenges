2921c4dca60071eafb58488c0853fcfbd151870a
wu-linghui <1540996580@qq.com>
2022-04-15 23:11:27 +0800

    parctice:11-easy-tuple-to-object

---
+++ type-challenges/easy/tuple-to-object/template.ts
@@ -0,0 +1,9 @@
+type TupleToObject<T extends readonly string[]> = {
+    [key in T[number]] : key;
+}
+
+/* 
+  1.typeof将js的转为ts类型
+  2.[key in T[number]]循环数组
+  3.[key in T] 循环对象
+ */
---
+++ type-challenges/easy/tuple-to-object/test-cases.ts
@@ -0,0 +1,25 @@
+import { Equal, Expect } from "@type-challenges/utils";
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type cases = [
+    Expect<
+        Equal<
+            TupleToObject<typeof tuple>,
+            {
+                tesla: "tesla";
+                "model 3": "model 3";
+                "model X": "model X";
+                "model Y": "model Y";
+            }
+        >
+    >
+];
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>;
+
+/* 
+  1.typeof将js的转为ts类型
+  2.
+ */
