ad5db278ddef499f380f31de40887c1a9bb8b934
wu-linghui <1540996580@qq.com>
2022-04-17 20:34:58 +0800

    practice：43-easy-exclude

---
+++ type-challenges/easy/exclude/template.ts
@@ -0,0 +1,7 @@
+type MyExclude<T, U> = T extends U ? never : T;
+type t1 = "a" | "b" | "c";
+type t2 = "a";
+type t3 = MyExclude<t1, t2>;
+/* 
+    1.T extends U ->先循环T和U配备三元是返回空集合never、否返回T不配备的类型
+ */
