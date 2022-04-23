5412d4e0b21a5418279da9dbe6a82bef422583af
wu-linghui <1540996580@qq.com>
2022-04-23 21:56:07 +0800

    practice:3057-easy-push

--- type-challenges/easy/if/template.ts
+++ type-challenges/easy/if/template.ts
@@ -1,5 +1,5 @@
 type If<C extends boolean, T, F> = C extends true ? T : F
 
-type t6 = If<null, 1, 2>
+// type t6 = If<null, 1, 2>
 
-let u: boolean = null;
+// let u: boolean = null;
---
+++ type-challenges/easy/push/template.ts
@@ -0,0 +1,1 @@
+type Push<T, U> = T extends any[] ? [...T, U] : never;
