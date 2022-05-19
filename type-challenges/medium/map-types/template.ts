3dbae5143e027dcab4933304b7b348ea9f8192c7
wu-linghui <1540996580@qq.com>
2022-05-19 22:24:13 +0800

    practice:05821-medium-map-types

---
+++ type-challenges/medium/map-types/template.ts
@@ -0,0 +1,16 @@
+type MapTypes<T, R extends Record<"mapTo" | "mapFrom", any>> = {
+    [key in keyof T] : T[key] extends R["mapFrom"]
+    ? R extends {"mapFrom": T[key]}
+      ? R["mapTo"]
+      : never
+    : T[key]
+}
+
+/* 
+    对于下面这个例子来说因为mapTo为联合类型会自动分发也就是计算2次不进行
+    ? R extends {"mapFrom": T[key]}
+      ? R["mapTo"]
+      : never
+    处理的话会返回string | boolean这一联合类型
+ */
+// type t14 = MapTypes<{ name: string; date: Date }, { mapFrom: string; mapTo: boolean } | { mapFrom: Date; mapTo: string }>
